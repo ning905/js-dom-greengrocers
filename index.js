f365eb691fac0d626a1111f9b7f2fcbffdded639
Ning Ma <ningma905@gmail.com>
2022-06-29 20:37:41 +0100

    add filter buttons

--- index.js
+++ index.js
@@ -3,51 +3,61 @@ const state = {
     {
       id: "001-beetroot",
       name: "beetroot",
+      type: "vegetable",
       price: 0.35,
     },
     {
       id: "002-carrot",
       name: "carrot",
+      type: "vegetable",
       price: 0.35,
     },
     {
       id: "003-apple",
       name: "apple",
+      type: "fruit",
       price: 0.35,
     },
     {
       id: "004-apricot",
       name: "apricot",
+      type: "fruit",
       price: 0.35,
     },
     {
       id: "005-avocado",
       name: "avocado",
+      type: "fruit",
       price: 0.35,
     },
     {
       id: "006-bananas",
       name: "bananas",
+      type: "fruit",
       price: 0.35,
     },
     {
       id: "007-bell-pepper",
       name: "bell pepper",
+      type: "vegetable",
       price: 0.35,
     },
     {
       id: "008-berry",
       name: "berry",
+      type: "fruit",
       price: 0.35,
     },
     {
       id: "009-blueberry",
       name: "blueberry",
+      type: "fruit",
       price: 0.35,
     },
     {
       id: "010-eggplant",
       name: "eggplant",
+      type: "vegetable",
       price: 0.35,
     },
   ],
@@ -58,8 +68,12 @@ const cartItemList = document.querySelector(".cart--item-list");
 const shopItemList = document.querySelector(".store--item-list");
 
 // Setters and getters
-const getItem = (itemName) => {
+const getItemFromShelf = (itemName) => {
   return state.items.find((item) => itemName === item.name);
+};
+
+const getItemFromCart = (itemName, cart = state.cart) => {
+  return cart.find((item) => itemName === item.name);
 };
 
 const setState = (updatedState) => {
@@ -70,7 +84,43 @@ const setState = (updatedState) => {
   render();
 };
 
-// Reusable functions
+// Reusable functions for event listeners
+const increaseQuantity = (itemName) => {
+  const myCart = [...state.cart];
+  const thisItem = getItemFromCart(itemName, myCart);
+  thisItem.quantity++;
+
+  setState({ cart: myCart });
+};
+
+const decreaseQuantity = (itemName) => {
+  let myCart = [...state.cart];
+  const thisItem = getItemFromCart(itemName, myCart);
+  thisItem.quantity--;
+
+  myCart = myCart.filter((item) => item.quantity > 0);
+
+  setState({ cart: myCart });
+};
+
+const addToCart = (itemName) => {
+  const myCart = [...state.cart];
+
+  const match = myCart.find((item) => itemName === item.name);
+
+  if (match) {
+    increaseQuantity(itemName);
+    return;
+  }
+
+  const thisItem = getItemFromShelf(itemName);
+  thisItem.quantity = 1;
+  myCart.push(thisItem);
+
+  setState({ cart: myCart });
+};
+
+// Reusable functions for creating elements
 const createImg = (item) => {
   const img = document.createElement("img");
   const imgSrc = `assets/icons/${item.id}.svg`;
@@ -92,6 +142,39 @@ const createCartBtn = () => {
   return btn;
 };
 
+// Header
+const header = document.querySelector("#store");
+
+const filterButtonContainer = document.createElement("div");
+header.insertBefore(filterButtonContainer, header.children[1]);
+filterButtonContainer.style.display = "flex";
+filterButtonContainer.style.justifyContent = "center";
+filterButtonContainer.style.margin = "1rem";
+filterButtonContainer.style.columnGap = "2rem";
+
+const fruitBtn = createButton();
+filterButtonContainer.appendChild(fruitBtn);
+fruitBtn.innerText = "Show Fruits";
+fruitBtn.addEventListener("click", (event) => {
+  const fruits = state.items.filter((item) => item.type === "fruit");
+  renderShelf(fruits);
+});
+
+const vegeBtn = createButton();
+filterButtonContainer.appendChild(vegeBtn);
+vegeBtn.innerText = "Show Vegetables";
+vegeBtn.addEventListener("click", (event) => {
+  const vegetable = state.items.filter((item) => item.type === "vegetable");
+  renderShelf(vegetable);
+});
+
+const allItemsBtn = createButton();
+filterButtonContainer.appendChild(allItemsBtn);
+allItemsBtn.innerText = "Show All Items";
+allItemsBtn.addEventListener("click", (event) => {
+  renderShelf();
+});
+
 // Shelf Item
 const createShopItem = (item) => {
   const li = document.createElement("li");
@@ -107,12 +190,18 @@ const createShopItem = (item) => {
   li.append(button);
   button.classList.add("add-to-cart-btn");
   button.innerText = "Add to cart";
+  button.addEventListener("click", (event) => {
+    const thisItemName = event.composedPath()[1].children[0].children[0].alt;
+    addToCart(thisItemName);
+  });
 
   return li;
 };
 
-const generateShelf = () => {
-  const myShelf = [...state.items];
+const renderShelf = (items = state.items) => {
+  shopItemList.innerHTML = "";
+
+  const myShelf = [...items];
 
   myShelf.forEach((item) => {
     shopItemList.append(createShopItem(item));
@@ -137,6 +226,10 @@ const createCartItem = (item) => {
   li.append(removeButton);
   removeButton.innerText = "-";
   removeButton.classList.add("remove-btn");
+  removeButton.addEventListener("click", (event) => {
+    const thisItemName = event.composedPath()[1].children[1].innerText;
+    decreaseQuantity(thisItemName);
+  });
 
   const quantityEl = document.createElement("span");
   li.append(quantityEl);
@@ -147,11 +240,15 @@ const createCartItem = (item) => {
   li.append(addButton);
   addButton.innerText = "+";
   addButton.classList.add("add-btn");
+  addButton.addEventListener("click", (event) => {
+    const thisItemName = event.composedPath()[1].children[1].innerText;
+    increaseQuantity(thisItemName);
+  });
 
   return li;
 };
 
-const generateCartList = () => {
+const renderCartList = () => {
   cartItemList.innerHTML = "";
 
   myCartItems = [...state.cart];
@@ -166,49 +263,31 @@ const generateCartList = () => {
   return cartItemList;
 };
 
+// Total cost
+const totalCostEl = document.querySelector(".total-number");
+
+const getItemCost = (item) => {
+  return item.price * item.quantity;
+};
+
+const getTotalCost = () => {
+  const myCart = [...state.cart];
+  let totalCost = 0;
+  for (const item of myCart) {
+    totalCost += getItemCost(item);
+  }
+  return totalCost;
+};
+
+const renderTotalCost = () => {
+  const totalCost = getTotalCost().toFixed(2);
+  totalCostEl.innerText = `£${totalCost}`;
+};
+
 const render = () => {
-  generateShelf();
-  generateCartList();
+  renderShelf();
+  renderCartList();
+  renderTotalCost();
 };
 
 render();
-
-// Reusable functions for event listeners
-const addToCart = (itemName) => {
-  const myCart = [...state.cart];
-
-  const match = myCart.find((item) => itemName === item.name);
-
-  if (match) {
-    match.quantity++;
-    setState({ cart: myCart });
-    console.log(myCart);
-    return;
-  }
-
-  const thisItem = getItem(itemName);
-  thisItem.quantity = 1;
-  myCart.push(thisItem);
-
-  setState({ cart: myCart });
-};
-
-// Add event listeners to all shelf buttons
-const addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
-
-const addListeners = (buttons) => {
-  buttons.forEach((btn) => addListener(btn));
-};
-
-const addListener = (btn) => {
-  btn.addEventListener("click", (event) => {
-    const thisItemName = event.path[1].children[0].children[0].alt;
-    console.log(thisItemName);
-    addToCart(thisItemName);
-  });
-};
-addListeners(addToCartButtons);
-
-// Add event listeners to cart buttons
-const addBtn = document.querySelector(".add-btn");
-const removeBtn = document.querySelector(".remove-btn");
